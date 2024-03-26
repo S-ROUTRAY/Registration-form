787826d72432a82d65f4c5c7cddc78193612f576
S-ROUTRAY <mrsmrutiranjanok@gmail.com>
2024-03-26 18:38:39 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
@@ -9,57 +9,63 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.b2mpkod.mongodb.net/registrationformDB`,{
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.b2mpkod.mongodb.net/registrationFormDB`,{
     serverSelectionTimeoutMS: 5000,
 });
 const registrationSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
+    name: String,
+    email: String,
+    password: String
 });
 
-const Registration = mongoose.model("Registration" , registrationSchema);
+const registration = mongoose.model("Registration", registrationSchema);
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/pages/index.html");
 })
 
 app.post("/register", async (req, res) => {
-    try{
-        const{name, email, password} = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-        const existingUser = await registration.findOne({email : email });
-if(!existingUser) {
-    const registrationData = new registration({
-            name, 
-            email,
-            password,
-        });
-        await registrationData.save(); 
-        res.redirect("/success");
+        const existingUser = await 
+        registration.findOne({ email: email });
+        if (!existingUser) {
+            const registrationData = new registration({
+                name,
+                email,
+                password,
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        }
+        else {
+            console.log("User alreadyexist");
+            res.redirect("/error");
+        }
+
     }
-    else{
-        console.log("User alreadyexist");
-        res.redirect("/error");
-    }
-    }
-    catch (error){
+    catch (error) {
         console.log(error);
-        res.redirect("error")
+        res.redirect("error");
+
     }
-})
-
-app.get("/success",(req, res)=>{
-    res.sendFile (__dirname+"/pages/success.html");
-})
-app.get("/error",(req, res)=>{
-    res.sendFile (__dirname+"/pages/error.html");
-})
+});
 
 
-app.listen(port,()=>{
+
+app.get("/success", (req, res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+})
+
+app.get("/error", (req, res) => {
+    res.sendFile(_dirname + "/pages/error.html");
+})
+
+
+app.listen(port, () => {
     console.log('server is running on port $ {port}');
 })
