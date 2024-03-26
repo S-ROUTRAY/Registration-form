746486b665193e8663b1e4890db906a0295bc06b
S-ROUTRAY <mrsmrutiranjanok@gmail.com>
2024-03-26 19:04:52 +0530

    first commit

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+
 const username = process.env.MONGODB_USERNAME;
-const password = process.env.MONGODB_PASSWORD
-mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.b2mpkod.mongodb.net/registrationFormDB`,{
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.b2mpkod.mongodb.net/registrationform`,{
     serverSelectionTimeoutMS: 5000,
 });
+
 const registrationSchema = new mongoose.Schema({
     name: String,
     email: String,
@@ -31,8 +34,7 @@ app.post("/register", async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
-        const existingUser = await 
-        registration.findOne({ email: email });
+        const existingUser = await registration.findOne({ email: email });
         if (!existingUser) {
             const registrationData = new registration({
                 name,
@@ -55,17 +57,12 @@ app.post("/register", async (req, res) => {
     }
 });
 
-
-
 app.get("/success", (req, res) => {
     res.sendFile(__dirname + "/pages/success.html");
 })
-
 app.get("/error", (req, res) => {
-    res.sendFile(_dirname + "/pages/error.html");
+    res.sendFile(__dirname + "/pages/error.html");
 })
-
-
 app.listen(port, () => {
-    console.log('server is running on port $ {port}');
+    console.log(`server is running on port ${port}`);
 })
